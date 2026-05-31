e5cc1d8dd161cc7541fb52b2e18baf71332ae319
Tirsa <tglafuente1998@ejemplo.com>
2026-05-31 21:50:31 +0200

    Correccion CORS despliegue

--- breakroom/src/main/java/com/breakroom/security/SecurityConfig.java
+++ breakroom/src/main/java/com/breakroom/security/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); 
+        configuration.setAllowedOrigins(Arrays.asList("https://tirsagarcialafuente.github.io")); 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); 
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); 
         configuration.setAllowCredentials(true); 
